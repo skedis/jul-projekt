bb788f7ebaee8760274f78b0707a236ce91e1e6a
Häckklipparen Janne <skedis123@gmail.com>
2019-12-11 00:12:25 +0100

    first second third fourth fith sixth seventh eighth ninth tenth

---
+++ app.js
@@ -0,0 +1,43 @@
+/* Christopher G 17TEK-S */
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const index = require('./routes/index');
+
+const app = express();
+const port = process.env.PORT || '8000';
+
+// Create database connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'julprojekt'
+});
+
+// Connect to database
+db.connect((err) => {
+  if (err) {
+    console.log('Failed to connect to database'); return;
+    // throw err;
+  }
+  console.log('Connected to database');
+});
+
+// Global for easier use in other files
+global.db = db;
+
+// Setting up static directory
+app.use(express.static(path.join(__dirname, '/public/')));
+
+// Setting up view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', index);
+
+// Start listening on port
+app.listen(port, () => {
+  console.log(`Listening on http://127.0.0.1:${port}`);
+});
