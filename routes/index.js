04c0ad84021677d904106f1d51138a34fe0b6a75
Häckklipparen Janne <skedis123@gmail.com>
2019-12-13 01:15:20 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Routes
 router.get('/', (req, res) => {
   // Getting all gifts from database
-  const getGiftsQuery = 'SELECT * FROM `gifts`';
+  const getGiftsQuery = 'SELECT * FROM gifts ORDER BY year DESC';
   db.query(getGiftsQuery, (err, result) => {
     if (err) throw err;
 
